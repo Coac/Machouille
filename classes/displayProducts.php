d87e60326f39d53c7629713022bc80da7558c516
SuperCoac <levictor3000@hotmail.fr>
2015-06-24 15:01:43 +0200

    Display products

--- classes/displayProducts.php
+++ classes/displayProducts.php
@@ -1,12 +1,27 @@
 <?php 
-require_once("classes/Database.class.php"); 
+require_once("classes/Product.class.php"); 
 
-$nbProduct
+$nbProductToDisplay = 10;
+
+$productObject = new Product();
+$products = $productObject->getAll();
+?>
+<div class="top-box">
+<?php
+for ($i=0; $i < $nbProductToDisplay && $i<count($products); $i++) {   
+	if($i%3 ==0) {
+?>
+		<div class="clear"></div>
+		</div>
+				
+		<div class="top-box">
+
+<?php 
+	}
 ?>
 
-<div class="top-box">
  <div class="col_1_of_3 span_1_of_3"> 
-   <a href="single.html">
+   <a href=<?php echo 'single.php?id=' . $products[$i]['id'] ?>>
 	<div class="inner_content clearfix">
 		<div class="product_image">
 			<img src="images/pic.jpg" alt=""/>
@@ -14,9 +29,9 @@ $nbProduct
         <div class="sale-box"><span class="on_sale title_shop">New</span></div>	
         <div class="price">
 		   <div class="cart-left">
-				<p class="title">Lorem Ipsum simply</p>
+				<p class="title"><?php echo $products[$i]['name'] ?></p>
 				<div class="price1">
-				  <span class="actual">$12.00</span>
+				  <span class="actual">$<?php echo $products[$i]['price'] ?></span>
 				</div>
 			</div>
 			<div class="cart-right"> </div>
@@ -25,45 +40,11 @@ $nbProduct
        </div>
      </a>
 	</div>
-   <div class="col_1_of_3 span_1_of_3">
-   	 <a href="single.html">
-		<div class="inner_content clearfix">
-		<div class="product_image">
-			<img src="images/pic1.jpg" alt=""/>
-		</div>
-        <div class="price">
-		   <div class="cart-left">
-				<p class="title">Lorem Ipsum simply</p>
-				<div class="price1">
-				  <span class="actual">$12.00</span>
-				</div>
-			</div>
-			<div class="cart-right"> </div>
-			<div class="clear"></div>
-		 </div>				
-       </div>
-       </a>
-	</div>
-	<div class="col_1_of_3 span_1_of_3">
-	 <a href="single.html">
-	  <div class="inner_content clearfix">
-		<div class="product_image">
-			<img src="images/pic2.jpg" alt=""/>
-		</div>
-        <div class="sale-box1"><span class="on_sale title_shop">Sale</span></div>	
-        <div class="price">
-		   <div class="cart-left">
-				<p class="title">Lorem Ipsum simply</p>
-				<div class="price1">
-				  <span class="reducedfrom">$66.00</span>
-				  <span class="actual">$12.00</span>
-				</div>
-			</div>
-			<div class="cart-right"> </div>
-			<div class="clear"></div>
-		 </div>				
-       </div>
-       </a>
-	</div>
-	<div class="clear"></div>
-</div>	
+
+
+<?php
+}
+?>
+<div class="clear"></div>
+</div>
+
