8254c54b67864cef8f98b4425bd670af495e852c
Maltiize <etienne.mart@laposte.net>
2015-06-24 16:23:14 +0200

    basket process

--- checkout.php
+++ checkout.php
@@ -1,8 +1,48 @@
+
+
+
 <?php $title = "Checkout"; include('partials/header.php'); ?>
          <div class="register_account">
            <div class="wrap">
-    	     <h4 class="title">Shopping cart is empty</h4>
-    	     <p class="cart">You have no items in your shopping cart.<br>Click<a href="index.html"> here</a> to continue shopping</p>
+    	     <?php
+    	     if(!isset($_SESSION["order"])){
+    	     	echo '<h4 class="title">Shopping cart is empty</h4>' ;
+    	     	echo  '<p class="cart">You have no items in your shopping cart.<br>Click<a href="index.html"> here</a> to continue shopping</p>';
+    	     }
+    	     else{
+    	     	$comp = 1;
+    	     	$arr =$_SESSION["order"];
+    	     	?>
+    	     	<h4 class="title">Your shopping cart</h4>
+    	     	<table class="table table-hover">
+			      <thead>
+			        <tr>
+			          <th>#</th>
+			          <th>Name product</th>
+			          <th>Quantity</th>
+			          <th></th>
+			        </tr>
+			      </thead>
+			    <tbody>
+			   <?php
+    	     	foreach ($arr as &$value) {
+   					?>
+   							<tr>
+					          <th scope='row'></th>
+					          <td><?php echo $value['name'] ?></td>
+					          <td><?php echo $value['qtt'] ?></td>
+					          <td><a href=<?php echo 'single.php?id=' . $value['id'] ?>> voir fiche produit</a></td>
+					        </tr> 
+				<?php 		}
+			
+				echo   '</tbody>
+					    </table>';
+
+    	     }
+
+
+    	     ?>
+
     	   </div>
     	</div>
   <?php include('partials/bottomPage.php'); ?>
