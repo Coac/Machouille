efbaed2b599e753394a313fb534af0c666c79c0b
Franck-le-Beauf <m1105.108.psg@gmail.com>
2015-06-24 10:53:37 +0200

    Bottom

---
+++ partials/bottomPage.php
@@ -0,0 +1,144 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Free Leoshop Website Template | Home :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="../css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="../css/form.css" rel="stylesheet" type="text/css" media="all" />
+<link href='http://fonts.googleapis.com/css?family=Exo+2' rel='stylesheet' type='text/css'>
+<script type="text/javascript" src="../js/jquery1.min.js"></script>
+<!-- start menu -->
+<link href="../css/megamenu.css" rel="stylesheet" type="text/css" media="all" />
+<script type="text/javascript" src="../js/megamenu.js"></script>
+<script>$(document).ready(function(){$(".megamenu").megamenu();});</script>
+<!--start slider -->
+<link rel="stylesheet" href="../css/fwslider.css" media="all">
+<script src="../js/jquery-ui.min.js"></script>
+<script src="../js/css3-mediaqueries.js"></script>
+<script src="../js/fwslider.js"></script>
+<!--end slider -->
+<script src="../js/jquery.easydropdown.js"></script>
+</head>
+<body>
+<div class="footer">
+  <div class="footer-top">
+    <div class="wrap">
+      <div class="section group example">
+        <div class="col_1_of_2 span_1_of_2">
+          <ul class="f-list">
+            <li><img src="../images/2.png"><span class="f-text">Frais de port Gratuits</span>
+              <div class="clear"></div>
+            </li>
+          </ul>
+        </div>
+        <div class="col_1_of_2 span_1_of_2">
+          <ul class="f-list">
+            <li><img src="../images/3.png"><span class="f-text">Contactez-nous | 0123456789</span>
+              <div class="clear"></div>
+            </li>
+          </ul>
+        </div>
+        <div class="clear"></div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-middle">
+    <div class="wrap">
+      <div class="section group example">
+        <div class="col_1_of_f_1 span_1_of_f_1">
+          <div class="section group example">
+            <div class="col_1_of_f_2 span_1_of_f_2">
+              <h3>Facebook</h3>
+              <script>(function(d, s, id) {
+						  var js, fjs = d.getElementsByTagName(s)[0];
+						  if (d.getElementById(id)) return;
+						  js = d.createElement(s); js.id = id;
+						  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+						  fjs.parentNode.insertBefore(js, fjs);
+						}(document, 'script', 'facebook-jssdk'));</script>
+              <div class="like_box">
+                <div class="fb-like-box" data-href="http://www.facebook.com/w3layouts" data-colorscheme="light" data-show-faces="true" data-header="true" data-stream="false" data-show-border="true"></div>
+              </div>
+            </div>
+            <div class="col_1_of_f_2 span_1_of_f_2">
+              <h3>From Twitter</h3>
+              <div class="recent-tweet">
+                <div class="recent-tweet-icon"> <span> </span> </div>
+                <div class="recent-tweet-info">
+                  <p>Ds which don't look even slightly believable. If you are <a href="#">going to use nibh euismod</a> tincidunt ut laoreet adipisicing</p>
+                </div>
+                <div class="clear"> </div>
+              </div>
+              <div class="recent-tweet">
+                <div class="recent-tweet-icon"> <span> </span> </div>
+                <div class="recent-tweet-info">
+                  <p>Ds which don't look even slightly believable. If you are <a href="#">going to use nibh euismod</a> tincidunt ut laoreet adipisicing</p>
+                </div>
+                <div class="clear"> </div>
+              </div>
+            </div>
+            <div class="clear"></div>
+          </div>
+        </div>
+        <div class="col_1_of_f_1 span_1_of_f_1">
+          <div class="section group example">
+            <div class="col_1_of_f_2 span_1_of_f_2">
+              <h3>Information</h3>
+              <ul class="f-list1">
+                <li><a href="#">Duis autem vel eum iriure </a></li>
+                <li><a href="#">anteposuerit litterarum formas </a></li>
+                <li><a href="#">Tduis dolore te feugait nulla</a></li>
+                <li><a href="#">Duis autem vel eum iriure </a></li>
+                <li><a href="#">anteposuerit litterarum formas </a></li>
+                <li><a href="#">Tduis dolore te feugait nulla</a></li>
+              </ul>
+            </div>
+            <div class="col_1_of_f_2 span_1_of_f_2">
+              <h3>Contactez-nous</h3>
+              <div class="company_address">
+                <p>500 rue Tropeasy</p>
+                <p>91530 FACILAND</p>
+                <p>FRANCE</p>
+                <p>Phone:(33) 123 456 789</p>
+                <p>Fax: (33) 987 654 321</p>
+                <p>Email: <span>mail[at]cfacile.com</span></p>
+              </div>
+              <div class="social-media">
+                <ul>
+                  <li> <span class="simptip-position-bottom simptip-movable" data-tooltip="Google"><a href="#" target="_blank"> </a></span></li>
+                  <li><span class="simptip-position-bottom simptip-movable" data-tooltip="Linked in"><a href="#" target="_blank"> </a> </span></li>
+                  <li><span class="simptip-position-bottom simptip-movable" data-tooltip="Rss"><a href="#" target="_blank"> </a></span></li>
+                  <li><span class="simptip-position-bottom simptip-movable" data-tooltip="Facebook"><a href="#" target="_blank"> </a></span></li>
+                </ul>
+              </div>
+            </div>
+            <div class="clear"></div>
+          </div>
+        </div>
+        <div class="clear"></div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom">
+    <div class="wrap">
+      <div class="copy">
+        <p>© 2015 Template by CFACILE</p>
+      </div>
+      <div class="f-list2">
+        <ul>
+          <li class="active"><a href="about.html">Qui sommes-nous ?</a></li>
+          |
+          <li><a href="delivery.html">Conditions Générales de Ventes</a></li>
+          |
+          <li><a href="delivery.html">Mentions légales</a></li>
+          |
+          <li><a href="contact.html">Contactez-nous</a></li>
+        </ul>
+      </div>
+      <div class="clear"></div>
+    </div>
+  </div>
+</div>
+</body>
+</html>
