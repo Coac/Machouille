5c26406587cfc6158e91df7e6057798da6ef2ec1
SuperCoac <levictor3000@hotmail.fr>
2015-06-25 10:57:19 +0200

    Fixed duplicate product cart

--- addProductCart.php
+++ addProductCart.php
@@ -1,16 +1,18 @@
 <?php
 $title = "404"; include('partials/header.php'); 
-
+$added = false;
 if(isset($_POST['id']) && isset($_POST['name']) && isset($_POST['quantity'])) {
 	if(isset($_SESSION['order'])) {
-	for ($i=0; $i < count($_SESSION['order']); $i++) { 
-		if($_SESSION['order'][$i]['id'] == $_POST['id']) {
-			$_SESSION['order'][$i]['qtt'] += $_POST['quantity'];
-			break;
-		}
+		for ($i=0; $i < count($_SESSION['order']); $i++) {
+			if($_SESSION['order'][$i]['id'] == $_POST['id']) {
+				$_SESSION['order'][$i]['qtt'] += $_POST['quantity'];
+				$added = true;
+				break;
+			}
 
-	}
-	array_push($_SESSION['order'], array("id"=>$_POST['id'],"qtt"=>$_POST['quantity'], "name"=>$_POST['name']) );
+		}
+		if($added == false)
+			array_push($_SESSION['order'], array("id"=>$_POST['id'],"qtt"=>$_POST['quantity'], "name"=>$_POST['name']) );
 	}
 	else
 		$_SESSION['order'][0] = array("id"=>$_POST['id'],"qtt"=>$_POST['quantity'], "name"=>$_POST['name']);
