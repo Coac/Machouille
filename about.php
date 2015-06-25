b78b45ed14fe90e1f9572cab2b10e7d13827c479
Franck-le-Beauf <m1105.108.psg@gmail.com>
2015-06-25 16:27:29 +0200

    on se casse

--- about.php
+++ about.php
@@ -27,7 +27,7 @@
 					   <div class="testimonials ">
 						<div class="testi-item">
 						<blockquote class="testi-item_blockquote">
-							<a href="#">Laissez moi faire les logos lol jsuis trop fort sur paint </a>
+							<a href="#">Jm'occupe du design, jsuis trop fort sur paint </a>
 							<div class="clear"></div>
 						</blockquote>
 							<small class="testi-meta"><span class="user">Frank le beauf</span>,
@@ -49,23 +49,22 @@
 								 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie.</p>
 								</div>
 							 </div>
-							 <h4 class="m_7"><a href="#">Etienne Martin : Chef de projet</a></h4>
-							 <p>Lorem ipsum dolor consectetur adipisicing elit, sed do eiusmod.</p>
+							 <h4 class="m_7"><a href="#">Etienne Martin : Chef de Rien</a></h4>
+							  
 					     </div>
 						<div class="col_1_of_about-box span_1_of_about-box">
 						   <a class="popup-with-zoom-anim" href="#small-dialog3">  <span class="rollover"></span><img src="images/chinois.png" title="continue" alt=""/></a>
-		                    <h4 class="m_7"><a href="#">Victor le developpeur de génie</a></h4>
-							 <p>Lorem ipsum dolor consectetur adipisicing elit, sed do eiusmod.</p>
+		                    <h4 class="m_7"><a href="#">Victor : developpeur de génie</a></h4>
+							 
 					     </div>
 						<div class="col_1_of_about-box span_1_of_about-box">
 						   <a class="popup-with-zoom-anim" href="#small-dialog3">  <span class="rollover"></span><img src="images/ui.jpg" title="continue" alt=""/></a>
-		                    <h4 class="m_7"><a href="#">Samy L'homme qu'il faut avaoir chez soi</a></h4>
-							 <p>Lorem ipsum dolor consectetur adipisicing elit, sed do eiusmod.</p>
+		                    <h4 class="m_7"><a href="#">Samy : l'homme qu'il faut avoir chez soi</a></h4>
 					     </div>
 					     <div class="col_1_of_about-box span_1_of_about-box">
 						   <a class="popup-with-zoom-anim" href="#small-dialog3">  <span class="rollover"></span><img src="images/ui.jpg" title="continue" alt=""/></a>
-		                    <h4 class="m_7"><a href="#">Frank le beauf #manquedimage</a></h4>
-							 <p>Lorem ipsum dolor consectetur adipisicing elit, sed do eiusmod.</p>
+		                    <h4 class="m_7"><a href="#">Adrian : designer creatif #manquedimage</a></h4>
+							 
 					     </div>
 				        <div class="clear"></div> 
 							
