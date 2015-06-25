4ee7dc8a34c500d06f603ecde0325636e837800f
Maltiize <etienne.mart@laposte.net>
2015-06-25 16:08:05 +0200

    about page

--- about.php
+++ about.php
@@ -8,31 +8,40 @@
 					  <div class="testimonials ">
 						<div class="testi-item">
 						<blockquote class="testi-item_blockquote">
-							<a href="#">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </a>
+							<a href="#">Ah mais c'est facile </a>
 							<div class="clear"></div>
 						</blockquote>
-							<small class="testi-meta"><span class="user">Lorem ipsum dolor </span>,
-							<span class="info">sit amet</span><br><a href="#">http://demolink.org</a></small></div>
+							<small class="testi-meta"><span class="user">Victor Le</span>,
+							</div>
 					   </div>
 					   <div class="testimonials ">
 						<div class="testi-item">
 						<blockquote class="testi-item_blockquote">
-							<a href="#">Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. </a>
+							<a href="#">Fuck Le web </a>
 							<div class="clear"></div>
 						</blockquote>
-							<small class="testi-meta"><span class="user">Lorem ipsum dolor</span>,
-							<span class="info">sit amet</span><br><a href="#">http://demolink.org</a></small></div>
+							<small class="testi-meta"><span class="user">Samy Abdelazim</span>,
+							</div>
+					   </div>
+
+					   <div class="testimonials ">
+						<div class="testi-item">
+						<blockquote class="testi-item_blockquote">
+							<a href="#">Laissez moi faire les logos lol jsuis trop fort sur paint </a>
+							<div class="clear"></div>
+						</blockquote>
+							<small class="testi-meta"><span class="user">Frank le beauf</span>,
+						</div>
 					   </div>
 				    </div>
 				    <div class="cont span_2_of_about">
 				       <h3>Our Mission</h3>
-					   	<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-					   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-				       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+					   	
+				       <p>Notre mission était de créer un site de gestion de commande de chewingum.</p>
 				        <h5 class="m_6">Meet Our Team</h5>	
 				     <div class="section group">
 						<div class="col_1_of_about-box span_1_of_about-box">
-						   <a class="popup-with-zoom-anim" href="#small-dialog3">  <span class="rollover"></span><img src="images/a-img.jpg" title="continue" alt=""/></a>
+						   <a class="popup-with-zoom-anim" href="#small-dialog3">  <span class="rollover"></span><img src="images/blanc.jpg" title="continue" alt=""/></a>
 		                     <div id="small-dialog3" class="mfp-hide">
 							   <div class="pop_up2">
 							   	 <h2>Lorem ipsum </h2>
@@ -40,17 +49,22 @@
 								 <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie.</p>
 								</div>
 							 </div>
-							 <h4 class="m_7"><a href="#">consectetuer adipiscing</a></h4>
+							 <h4 class="m_7"><a href="#">Etienne Martin : Chef de projet</a></h4>
 							 <p>Lorem ipsum dolor consectetur adipisicing elit, sed do eiusmod.</p>
 					     </div>
 						<div class="col_1_of_about-box span_1_of_about-box">
-						   <a class="popup-with-zoom-anim" href="#small-dialog3">  <span class="rollover"></span><img src="images/a-img1.jpg" title="continue" alt=""/></a>
-		                    <h4 class="m_7"><a href="#">consectetuer adipiscing</a></h4>
+						   <a class="popup-with-zoom-anim" href="#small-dialog3">  <span class="rollover"></span><img src="images/chinois.png" title="continue" alt=""/></a>
+		                    <h4 class="m_7"><a href="#">Victor le developpeur de génie</a></h4>
 							 <p>Lorem ipsum dolor consectetur adipisicing elit, sed do eiusmod.</p>
 					     </div>
 						<div class="col_1_of_about-box span_1_of_about-box">
-						   <a class="popup-with-zoom-anim" href="#small-dialog3">  <span class="rollover"></span><img src="images/a-img2.jpg" title="continue" alt=""/></a>
-		                    <h4 class="m_7"><a href="#">consectetuer adipiscing</a></h4>
+						   <a class="popup-with-zoom-anim" href="#small-dialog3">  <span class="rollover"></span><img src="images/ui.jpg" title="continue" alt=""/></a>
+		                    <h4 class="m_7"><a href="#">Samy A L'homme qu'il faut avaoir chez soi</a></h4>
+							 <p>Lorem ipsum dolor consectetur adipisicing elit, sed do eiusmod.</p>
+					     </div>
+					     <div class="col_1_of_about-box span_1_of_about-box">
+						   <a class="popup-with-zoom-anim" href="#small-dialog3">  <span class="rollover"></span><img src="images/ui.jpg" title="continue" alt=""/></a>
+		                    <h4 class="m_7"><a href="#">Frank le beauf #manquedimage</a></h4>
 							 <p>Lorem ipsum dolor consectetur adipisicing elit, sed do eiusmod.</p>
 					     </div>
 				        <div class="clear"></div> 
